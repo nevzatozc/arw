70a7712257230989216eb0e6c1730320582a88ce
nevzatozc <47524864+nevzatozc@users.noreply.github.com>
2020-08-14 09:42:10 +0300

    Update Blockchain.js

--- Blockchain.js
+++ Blockchain.js
@@ -38,7 +38,13 @@ function getDBBlockHeight() {
     });
 }
 let errorLog = [];
-
+var num0fvalidblock=0;
+var num0finvalidblock=0;
+var invalidblockarray =[];
+var validblockarray =[];
+var invalidblockarrayheight =[];
+var validblockarrayheight =[];
+let allblocks = [];
 class Blockchain{
     constructor(isim){
         this.ad = isim;
@@ -86,22 +92,32 @@ class Blockchain{
         //newBlock.recall_block = rn % this.total_chain_height;
         //console.log("added block height: " + newBlock.height + " hash: " + newBlock.hash.toString() + " recall: " + newBlock.recall_block + " rand: "+rn +" crr tch: "+this.total_chain_height )
 
-        // var tmp_recall_arr = []
-        var recal_arr_size = Math.ceil(Math.log2(this.total_chain_height));
+        //var recal_arr_size = Math.ceil(Math.log2(this.total_chain_height));
+        var recal_arr_size = Math.ceil(Math.sqrt(this.total_chain_height));
 
-        let initial_recall_data = {
-            recall_id:(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)) % this.total_chain_height,
+        /*var recal_arr_size = 3;//Math.ceil(Math.log2(this.total_chain_height));
+        if(newBlock.height===0)
+             recal_arr_size = 0;
+        else if(newBlock.height===1 )
+            recal_arr_size = 1;
+        else
+            recal_arr_size = 3;
+        *///var recal_arr_size = 2;//Math.ceil(Math.log2(this.total_chain_height));
+        newBlock.recall_arr.push(newBlock.height);
+        recal_arr_size--;
+        //let initial_recall_data = {
+        var recall_id= newBlock.height-1;//(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)) % this.total_chain_height;
             //flag:false,
-        };
-        newBlock.recall_arr.push(initial_recall_data);
+
+        newBlock.recall_arr.push(recall_id);
         recal_arr_size--;
 
         lbl: while (recal_arr_size > 0) {
-            let recall_block_height =  (Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)) % this.total_chain_height;
-            recall_block_height = this.total_chain_height - recall_block_height - 1;
+            let recall_block_height =  (Math.ceil(Math.random() * Number.MAX_SAFE_INTEGER)) % this.total_chain_height;
+            recall_block_height = this.total_chain_height - recall_block_height -1;
             let flag = true;
             for (let i = 0; i < newBlock.recall_arr.length; i++) {
-                if(recall_block_height === newBlock.recall_arr[i].recall_id ){
+                if(recall_block_height === newBlock.recall_arr[i] ){
                     // continue lbl;
                     flag = false;
                 }
@@ -110,8 +126,9 @@ class Blockchain{
             if (!flag) {
                 continue lbl;
             } else {
-                let recall_data = { recall_id: recall_block_height, //flag:false
-                };
+                //let recall_data = { recall_id: recall_block_height, //flag:false
+                //};
+                let recall_data = recall_block_height;
                 newBlock.recall_arr.push(recall_data);
             }
 
@@ -119,142 +136,129 @@ class Blockchain{
         }
         // newBlock.recall_arr = tmp_recall_arr;
         ////
+        /*var iscurrentblockvalid = (Math.round(Math.random() * Number.MAX_SAFE_INTEGER)) % 200;
 
-        console.log("curr h: "+this.total_chain_height+" Logarithm size: " + newBlock.recall_arr.length + " recall_array: " + JSON.stringify(newBlock.recall_arr) )
-        // console.log("added block height: " + newBlock.height + " hash: " + newBlock.hash.toString() + " recall_arr: " + JSON.stringify(newBlock.recall_arr) )
+        if (iscurrentblockvalid)
+        {
+            newBlock.isvalid = true;
+            validblockarray.push(newBlock);
+            num0fvalidblock++;
+        }
+        else {
+            newBlock.isvalid = false;
+            invalidblockarray.push(newBlock);
+            num0finvalidblock++
+        }*/
+        //console.log("curr h: "+this.total_chain_height+" Logarithm size: " + newBlock.recall_arr.length + " recall_array: " + JSON.stringify(newBlock.recall_arr) + " isBlockvalid: "+ newBlock.isvalid )
+        //console.log("added block height: " + newBlock.height + " hash: " + newBlock.hash.toString() + " recall_arr: " + JSON.stringify(newBlock.recall_arr) )
+        allblocks.push(newBlock.height);
         await addDataToLevelDB(newBlock.height, JSON.stringify(newBlock));
-        console.log("Block " + (newBlock.height)+" added to DB");
+        //console.log("Block " + (newBlock.height)+" added to DB");
         return newBlock.height;
     }
-    async validateBlock(blockHeight) {
-        // get block object
-        const block =  await this.getBlock(blockHeight);
-        // get block hash
-        let blockHash = block.hash;
-        // remove block hash to test block integrity
-        block.hash = "";
-        var checkPoint= JSON .stringify(block);
-        // generate block hash
-        let currentBlockHash = SHA256(checkPoint).toString();
-        //console.log('!!!blockHash= ' + blockHash +' & ' + 'validBlockHash= ' + validBlockHash);
-        // Compare
-        if (blockHash === currentBlockHash) {
-            console.log('Block #' + blockHeight + ' validated'+ " Body : " + block.body);
-            return true;
-        } else {
-            console.log('Block #' + blockHeight + ' has invalid hash '+ " Body : " + block.body);
-            //console.log('blockHash= ' + blockHash +' & ' + 'validBlockHash= ' + validBlockHash);
-            //console.log('???block '+ block.height + '  height: '+ block.height +'  Hash:'+ block.hash+ ' Time: ' +block.time + ' Body: '+checkPoint);
-            throw "Block check error "+ blockHeight;
-        }
-    }
-    /**validate chain functions****/
 
-    async validateEachBlock(blockHeight) {
-
-        for (let i = 0; i <= blockHeight; i++) {
-            try {
-                await this.validateBlock(i);
-            } catch (err) {
-                console.log('Error in validateBlockIntegrity ', err);
-                errorLog.push(i);
-            }
-        }
-        return errorLog;
-    }
-
-    async validateChainIntegrity(blockHeight) {
-        //let errorLog = [];
-        let currentBlock;
-        let prevBlock;
-        for (let i = 1; i <= blockHeight; i++) {
-
-            currentBlock =  await this.getBlock(i);
-            prevBlock =  await this.getBlock(i-1);
-            if (currentBlock.previousBlockHash !== prevBlock.hash) {
-                console.log("Previous Hash ERROR in validatechainIntegrity for Block " + i);
-                errorLog.push(i);
-            }
-            else
-                console.log("Hash is correct for Block " + i + " Body : " + currentBlock.body);
-        }
-        return errorLog;
-    }
-
-    async validateChain() {
-        const blockHeight =  await this.getBlockHeight();
-
-        let errorLog_1=[];
-        errorLog_1=  await this.validateChainIntegrity(blockHeight-1);
-
-        errorLog.concat(errorLog_1);
-
-        //errorLog =  await this.validateEachBlock(blockHeight-1);
-        errorLog.concat(errorLog_1);
-        return errorLog;
-    }
-    async validateB() {
-        const blockHeight =  await this.getBlockHeight();
-
-        let errorLog_1=[];
-
-        errorLog =  await this.validateEachBlock(blockHeight-1);
-        errorLog.concat(errorLog_1);
-        return errorLog;
-    }
-    async  guncelle(key){
-        var blok = await bc.getBlock(key);
-        blok.body = "Nevza"+key;
-        await bc.updateBlock(key, blok);
-    }
-    async  guncelle2(key){
-        var blok = await bc.getBlock(key);
-        blok.previousBlockHash = "nevzatnevzatnevzat";
-        await bc.updateBlock(key, blok);
-    }
     async  updateBlock(key, blok){
         addDataToLevelDB(key, JSON.stringify(blok));
-        console.log("Block " + (key)+" updated to DB");
+        //console.log("Block " + (key)+" updated to DB");
         return key;
     }
 
 }
 let bc = new Blockchain();
 const kayitEkle = (name) => bc.addBlock(new BlockClass.StarBlock(name))
-
+var numoftestblocks=400000-1;
+var numof_grouped_blocks_by_one_node=20000;
+var invalidity_percentage= 0.25; //numoftestblocks*invalidity_percentage/100
+var enough_invalid=(numoftestblocks/100)*invalidity_percentage;//integer invalid block sayisi
 
 const main = async ()=> {
-
-    for (var i = 0; i < 200; i++) {
+    for (var i = 0; i < numoftestblocks; i++) {
         const data1Result = await kayitEkle("Data"+(i+1));
     }
-    /*await bc.guncelle2(8);
-    const sonuc = await bc.getBlock(8);
-    console.log(sonuc)
-    let errlog = await bc.validateChain();
-    if (errlog.length > 0) {
-        console.log('Block errors = ' + errorLog.length);
-        console.log('Blocks: ' + errlog);
-    } else {
-        console.log('No errors detected');
-    }
-    console.log("Chain analysis result " + errlog.toString());*/
+    var k=0;
+    var j= 0,y=0;
+    var catchedarr =[];
     //
-    /*let errlog2 = await bc.validateB();
-    if (errlog2.length > 0) {
-        console.log('Block errors = ' + errorLog.length);
-        console.log('Blocks: ' + errlog2);
-    } else {
-        console.log('No errors detected22');
+    let currentBlock;
+    var ii=0;
+    var num_of_inv_blck=0;
+    while( ii< enough_invalid){
+        var makethisblockinvalid = (Math.round(Math.random() * Number.MAX_SAFE_INTEGER)) % numoftestblocks;
+        currentBlock =  await bc.getBlock(makethisblockinvalid);
+        if (currentBlock.height < 3)
+            continue;
+        if(!(invalidblockarrayheight.includes(currentBlock.height)))
+        {
+            currentBlock.isvalid = false;
+            invalidblockarray.push(currentBlock);
+            invalidblockarrayheight.push(currentBlock.height);
+            await bc.updateBlock(currentBlock.height, currentBlock);
+            num0finvalidblock++;
+            ii++;
+        }
     }
-    console.log("Chain analysis result2 " + errlog2.toString())*/
+    let z = 0;
+    while (z <= numoftestblocks){
+        if(!invalidblockarrayheight.includes(allblocks[z]))
+        {
+            currentBlock =  await bc.getBlock(allblocks[z]);
+            validblockarray.push(currentBlock);
+            validblockarrayheight.push(allblocks[z]);
+            num0fvalidblock++;
+        }
+        z++;
+    }
+    //group blocks by a node
+    var groupedblockarray =[];
+    for (let i = 0; i < numof_grouped_blocks_by_one_node; i++) {
+        var randomformod = Math.floor(Math.random() * numoftestblocks) + 1;
+        currentBlock =  await bc.getBlock(randomformod);
+        if (currentBlock.height < 3)
+        {
+            i--;
+            continue;
+        }
+        if (!groupedblockarray.includes(randomformod))
+        {
+            groupedblockarray.push(currentBlock);
+        }
+    }
+    let groupedblockarrayh =[];
+    for (var i = 0; i < groupedblockarray.length; i++) {
+         groupedblockarrayh[i] = groupedblockarray[i].height;
+    }
+    //console.log("g height:"+groupedblockarrayh.length+" group block arr: "+groupedblockarrayh);
     //
+    //console.log("bozma bitti")
+    console.log("num of total blocks: "+ allblocks.length +" numofvalidBlock: "+num0fvalidblock+ " ||numofinvalidBlock: "+ num0finvalidblock+" ||num of block that a node contains: "+groupedblockarrayh.length  );
+    //console.log("valid block: " + validblockarrayheight);
+    //console.log("invalid block: " + invalidblockarrayheight);
+    while(j< groupedblockarray.length)
+    {
+        //console.log("no of the block j: "+j+" arr l: "+validblockarray[j].recall_arr.length)
+        while(k < groupedblockarray[j].recall_arr.length ) {
+
+            while (y < invalidblockarray.length ) {
+                //console.log("i am at this block:"+j+ "and my recall arry l is: "+k);
+                if (groupedblockarray[j].recall_arr[k] === invalidblockarrayheight[y]) //validblockarray[y].height
+                {
+                    //console.log("Bu numaralı block: "+validblockarray[j].height+"bunu yakaladı: "+ invalidblockarrayheight[y])
+                    if(!catchedarr.includes(invalidblockarrayheight[y]))
+                        catchedarr.push(invalidblockarrayheight[y]);
+                }
+                y++;
+            }
+            y=0;
+            k++;
+        }
+        k=0;
+        j++;
+    }
+    console.log("% bozukluk oranı: "+(invalidblockarray.length/(allblocks.length))*100)
+    console.log("% yakalama orani: "+ (catchedarr.length/invalidblockarray.length)*100)
+    console.log("bozuk block sayisi: "+invalidblockarray.length+ " | yaklanan sayi: "+catchedarr.length);
+
 }
 
-main()
-/*async function hashModulo(hash, operand) {
-    hex_hash = "0x" + hash;
-    console.log(hex_hash);
-    return Number(BigInt(hash) % BigInt(operand));
-}*/
+main();
 module.exports.Blockchain = Blockchain;
